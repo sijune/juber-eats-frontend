39353e7ffc16f279e1424145513b996517b3f4f2
sijune <sijune0525@gmail.com>
2021-10-15 00:54:25 +0900

    option

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
         'react/prop-types': 0,
         'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
         'jsx-a11y/no-noninteractive-element-interactions': 0,
+        '@typescript-eslint/ban-ts-comment': 'off',
     },
 };
--- src/pages/owner/add-dish.tsx
+++ src/pages/owner/add-dish.tsx
@@ -7,6 +7,7 @@ import { Button } from '../../components/button';
 import { useHistory } from 'react-router-dom';
 import { MY_RESTAURANT_QUERY } from './my-restaurant';
 import { createDish, createDishVariables } from '../../__generated__/createDish';
+import React, { useState } from 'react';
 
 const CREATE_DISH_MUTATION = gql`
   mutation createDish($input: CreateDishInput!) {
@@ -25,6 +26,7 @@ interface IForm {
   name: string;
   price: string;
   description: string;
+  [key: string]: string;
 }
 
 export const AddDish = () => {
@@ -42,13 +44,20 @@ export const AddDish = () => {
         },
       },
     ],
+    onCompleted: () => {
+      console.log('#####');
+    },
   });
 
-  const { register, handleSubmit, formState, getValues } = useForm<IForm>({
+  const { register, handleSubmit, formState, getValues, setValue } = useForm<IForm>({
     mode: 'onChange',
   });
   const onSubmit = () => {
-    const { name, price, description } = getValues();
+    const { name, price, description, ...rest } = getValues();
+    const optionObjects = optionsNumber.map((theId) => ({
+      name: rest[`${theId}-optionName`], //rest object 중에서 id-optionName으로 값을 가져온다.
+      extra: +rest[`${theId}-optionExtra`], //rest object 중에서 id-optionExtra으로 값을 가져온다.
+    }));
     createDishMutation({
       variables: {
         input: {
@@ -56,10 +65,21 @@ export const AddDish = () => {
           price: +price,
           description,
           restaurantId: +restaurantId,
+          options: optionObjects,
         },
       },
     });
     history.goBack();
+  };
+  
+  const [optionsNumber, setOptionsNumber] = useState<number[]>([]); //<>안에 배열로 들어갈 값을 정의해줘야 한다.
+  const onAddOptionClick = () => {
+    setOptionsNumber((current) => [Date.now(), ...current]);
+  };
+  const onDeleteClick = (idToDelete: number) => {
+    setOptionsNumber((current) => current.filter((id) => id !== idToDelete)); //난수 배열에서 제거
+    setValue(`${idToDelete}-optionName`, ''); //Form에서 제거
+    setValue(`${idToDelete}-optionExtra`, '');
   };
 
   return (
@@ -94,6 +114,36 @@ export const AddDish = () => {
           className="input"
           placeholder="Description"
         />
+        <div className="my-10">
+          <h4 className="font-medium mb-3 text-lg">Dish Options</h4>
+          <span className="cursor-pointer text-white bg-gray-900 py-1 px-2 mt-5" onClick={onAddOptionClick}>
+            Add Dish Option
+          </span>
+          {optionsNumber.length !== 0 &&
+            optionsNumber.map((id) => (
+              <div key={id} className="mt-5">
+                <input
+                  {...register(`${id}-optionName`)}
+                  type="text"
+                  placeholder="Option Name"
+                  className="py-2 px-4 focus:outline-none mr-3 focus:border-gray-600 border-2"
+                />
+                <input
+                  {...register(`${id}-optionExtra`)}
+                  type="number"
+                  placeholder="Option Extra"
+                  className="py-2 px-4 focus:outline-none focus:border-gray-600 border-2"
+                  min={0}
+                />
+                <span
+                  className="cursor-pointer text-white bg-red-500 ml-3 py-3 px-4 mt-5"
+                  onClick={() => onDeleteClick(id)}
+                >
+                  Delete option
+                </span>
+              </div>
+            ))}
+        </div>
         <Button loading={loading} canClick={formState.isValid} actionText="Create Dish" />
       </form>
     </div>
