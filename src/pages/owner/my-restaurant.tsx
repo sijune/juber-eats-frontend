9bd40898d6a5bfbf4dcc5dae17cad3bb3e629190
sijune <sijune0525@gmail.com>
2021-10-15 23:52:07 +0900

    vicotry

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -4,8 +4,10 @@ import gql from 'graphql-tag';
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
-import { myRestaurant, myRestaurantVariables } from '../../__generated__/myRestaurant';
 import { Helmet } from 'react-helmet-async';
+import { myRestaurant, myRestaurantVariables } from '../../__generated__/myRestaurant';
+import { Dish } from '../../components/dish';
+import { VictoryChart, VictoryAxis, VictoryBar } from 'victory';
 
 export const MY_RESTAURANT_QUERY = gql`
   query myRestaurant($input: MyRestaurantInput!) {
@@ -60,7 +62,30 @@ export const MyRestaurant = () => {
         <div className="mt-10">
           {data?.myRestaurant.restaurant?.menu.length === 0 ? (
             <h4 className="text-xl mb-5">Please upload a dish!</h4>
-          ) : null}
+          ) : (
+            <div className="grid md:grid-cols-3 gap-x-5 gap-y-10 mt-16">
+              {data?.myRestaurant.restaurant?.menu.map((dish) => (
+                <Dish name={dish.name} description={dish.description} price={dish.price} />
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="mt-20 mb-10">
+          <h4 className="text-center text-2xl font-medium">Sales</h4>
+          <div className="max-w-lg w-full mx-auto">
+            <VictoryChart domainPadding={20}>
+              <VictoryAxis label="Amount of Money" dependentAxis tickValues={[20, 30, 40, 50, 60]} />
+              <VictoryAxis label="Days of Life" />
+              <VictoryBar
+                data={[
+                  { x: 10, y: 20 },
+                  { x: 20, y: 5 },
+                  { x: 35, y: 55 },
+                  { x: 45, y: 99 },
+                ]}
+              />
+            </VictoryChart>
+          </div>
         </div>
       </div>
     </div>
